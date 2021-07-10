81636a17899cc0ed202a1bc63a56c0d6573998d3
Daesung Hwang <hds2g22@gmail.com>
2021-07-10 05:52:47 +0000

    reports model & first view and template

--- src/sales/views.py
+++ src/sales/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(request):
+    message = 'hello from sales'
+    return render(request, 'sales/main.html', {'message':message})
+
